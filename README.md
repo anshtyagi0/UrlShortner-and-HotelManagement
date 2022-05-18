944a4280641ec43ed7321aae87c2bb7569bb5d16
Ansh Tyagi <anshtyagi0093@gmail.com>
2022-05-18 16:06:04 +0530

    UPLOADED FROM MAC

---
+++ README.md
@@ -0,0 +1,28 @@
+# Project by Ansh Tyagi & Itisha Singh
+
+<p>This is a url shortner and a Hotel Management system which is fully operational and user friendly.This system uses Mysql as database.<br>
+  This system is based on flask as it is a web based system. You can use your own domain to connect to this but as it is only a project we recommand you to do run without domain.</p>
+
+## How to use this
+**Download Python:** [Download](https://python.org/download)
+<h4>Install Modules</h4>
+
+```terminal
+pip install -r requirements.txt
+```
+<h4>Running</h4>
+
+```terminal
+python main.py
+```
+This will start your app on ```127.0.0.1:10000```, ```localhost:10000```
+
+Now you can just use it.
+
+# Disclaimer
+This project is only for education purpose and not for commercial use. We will not be responsible for anything. If you are using it then just don't just copy it first learn who it is working and then use it. And also don't forget to give use credit or it can cause a takedown request.
+
+
+## Credit
+```Ansh Tyagi, Itisha Singh```
+##
---
+++ main.py
@@ -0,0 +1,445 @@
+# Importing modules
+from flask import Flask, request, jsonify, redirect, render_template
+import pymysql
+import shortuuid
+
+# defining url for website & table name & defining template folders.
+baseurl = 'http://127.0.0.1:10000'
+table_name = 'urls'
+app = Flask(__name__, template_folder='templates')
+
+# Setting up mysql connection
+
+
+def connection():
+    connection = pymysql.connect(host='localhost', user='root', password='123456',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+# Main page of site
+
+
+@app.route('/')
+def render():
+    return render_template('index.html')
+
+# Shortner page
+
+
+@app.route('/shortner', methods=['GET', 'POST'])
+def shorten():
+    if request.method == 'POST':
+        LINK = request.form['url']
+        SHORT = request.form['custom']
+        if not SHORT:
+            SHORT = shortuuid.ShortUUID().random(length=7)
+        if not LINK:
+            return jsonify({'ERROR': 'Please give link'})
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        createtable = """CREATE TABLE IF NOT EXISTS urls(id INT AUTO_INCREMENT PRIMARY KEY,LINK TEXT,SHORT TEXT);"""
+        mydb.execute(createtable)
+        query2 = f"SELECT SHORT FROM urls WHERE SHORT = '{SHORT}'"
+        mydb.execute(query2)
+        try:
+            U = mydb.fetchone()['SHORT']
+            if U:
+                return jsonify({"Error": 'That short code is already in use.'})
+            query = 'INSERT INTO urls (LINK, SHORT) VALUES(%s, %s)'
+            mydb.execute(query, (LINK, SHORT))
+            conn.commit()
+            return render_template('short.html', short_url=baseurl+'/'+SHORT)
+        except:
+            query = 'INSERT INTO urls (LINK, SHORT) VALUES(%s, %s)'
+            mydb.execute(query, (LINK, SHORT))
+            conn.commit()
+            return render_template('short.html', short_url=baseurl+'/'+SHORT)
+    return render_template('short.html')
+
+# hotel management page
+
+
+@app.route('/hotel')
+def hotelpage():
+    return render_template("hotel.html")
+
+# hotel customer details
+
+
+@app.route('/hotel/customerdetails', methods=['GET', 'POST'])
+def customerdetails():
+    if request.method == 'POST':
+        createTable = """CREATE TABLE IF NOT EXISTS customer_details(CID VARCHAR(20) PRIMARY KEY,NAME VARCHAR(30),ADDRESS VARCHAR(30),AGE INT,NATIONALITY VARCHAR(30) ,PHONENO CHAR(10),EMAIL VARCHAR(30));"""
+        cid = request.form['cid']
+        name = request.form['name']
+        address = request.form['address']
+        age = request.form['age']
+        nationality = request.form['nationality']
+        phoneno = request.form['phoneno']
+        email = request.form['email']
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        mydb.execute(createTable)
+        try:
+            query1 = f"""SELECT CID FROM customer_details WHERE CID = {cid}"""
+            mydb.execute(query1)
+            u=mydb.fetchone()['CID']
+            return jsonify({'error': f'Customer Id : {cid} is already in use.'})
+        except:
+            query = """INSERT INTO customer_details(cid, name, address,age,nationality,phoneno,email) VALUES('{}','{}','{}','{}','{}','{}','{}')""".format(cid, name, address, age, nationality, phoneno, email)
+            mydb.execute(query)
+            conn.commit()
+            return render_template('customerdetails.html', prin="New Customer added to data.")
+    return render_template('customerdetails.html')
+
+# hotel bookings
+
+
+@app.route('/hotel/booking', methods=['GET', 'POST'])
+def booking():
+    if request.method == 'POST':
+        createTable = """CREATE TABLE IF NOT EXISTS BOOKING(CID VARCHAR(20) PRIMARY KEY,CHECK_IN DATE,CHECK_OUT DATE);"""
+        cid = request.form['cid']
+        checkin = request.form['checkin']
+        checkout = request.form['checkout']
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        mydb.execute(createTable)
+        try:
+            query1 = f"""SELECT CID FROM BOOKING WHERE CID = {cid}"""
+            mydb.execute(query1)
+            u=mydb.fetchone()['CID']
+            return render_template('bookings.html', prin="We already have booking at that customer id.")
+        except:
+            query = """INSERT INTO booking(cid,check_in,check_out) VALUES('{}','{}','{}')""".format(cid, checkin, checkout)
+            mydb.execute(query)
+            conn.commit()
+            return render_template('bookings.html', prin="New Booking added.")
+    return render_template("bookings.html")
+
+# hotel room rent
+
+
+@app.route('/hotel/roomrent', methods=['GET', 'POST'])
+def roomrent():
+    if request.method == 'POST':
+        createTable = """CREATE TABLE IF NOT EXISTS ROOM_RENT(CID VARCHAR(20) PRIMARY KEY,ROOM_CHOICE INT,NO_OF_DAYS INT,ROOM_NO INT ,ROOMBILL INT);"""
+        cid = request.form['cid']
+        room_choice = int(request.form['roomchoice'])
+        print(room_choice)
+        room_no = int(request.form['roomno'])
+        no_of_days = int(request.form['nodays'])
+        if room_choice == 1:
+            roomrent = no_of_days*2000
+        elif room_choice == 2:
+            roomrent = no_of_days*4000
+        elif room_choice == 3:
+            roomrent = no_of_days*6000
+        elif room_choice == 4:
+            roomrent = no_of_days*8000
+        else:
+            return render_template("roomrent.html", prin="Wrong Room Choice")
+        query = """INSERT INTO room_rent(cid,room_choice,room_no,no_of_days,roombill)VALUES(' {}','{}','{}','{}','{}')""".format(
+            cid, room_choice, room_no, no_of_days, roomrent)
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        mydb.execute(createTable)
+        try:
+            query1 = f"""SELECT CID FROM ROOM_RENT WHERE CID = {cid}"""
+            mydb.execute(query1)
+            u=mydb.fetchone()['CID']
+            return jsonify({'error': f'Customer already has a room on this Id.'})
+        except:
+            mydb.execute(query)
+            conn.commit()
+            return render_template("roomrent.html", prin=f"Thanks! Your Room is booked for {no_of_days} days and total room rent is Rs.{roomrent}/-")
+    return render_template("roomrent.html")
+
+# Hotel Restaurant
+
+
+@app.route("/hotel/restaurant", methods=['GET', 'POST'])
+def restaurant():
+    if request.method == 'POST':
+        createTable = """CREATE TABLE IF NOT EXISTS RESTAURANT(CID VARCHAR(20) PRIMARY KEY,MEAL_CHOICE INT,QUANTITY INT,RESTAURANT_BILL INT);"""
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        mydb.execute(createTable)
+        cid = request.form['cid']
+        meal_choice = int(request.form.get("choose"))
+        quantity = int(request.form.get("quantity"))
+        print(meal_choice)
+        print(quantity)
+
+        if meal_choice == 1:
+            restaurant_bill = quantity*300
+        elif meal_choice == 2:
+            restaurant_bill = quantity*500
+        else:
+            return render_template("restaurant.html", prin="Wrong meal choice.")
+
+        query = """INSERT INTO restaurant(cid,meal_choice,quantity,restaurant_bill)VALUES('{}','{}','{}',' {}')""".format(
+            cid, meal_choice, quantity, restaurant_bill)
+        try:
+            query1 = f"""SELECT CID FROM RESTAURANT WHERE CID = {cid}"""
+            mydb.execute(query1)
+            u=mydb.fetchone()['CID']
+            query1 = f"""SELECT RESTAURANT_BILL FROM RESTAURANT WHERE CID = {cid}"""
+            mydb.execute(query1)
+            u=mydb.fetchone()['RESTAURANT_BILL']
+            query = f"""UPDATE RESTAURANT SET RESTAURANT_BILL = {u+restaurant_bill} WHERE CID = {cid}"""
+            mydb.execute(query)
+            conn.commit()
+            return render_template("restaurant.html", prin=f"Your total bill for meal is Rs.{restaurant_bill}/- and total restaurant bill is Rs.{u+restaurant_bill} (including old)")
+        except:
+            mydb.execute(query)
+            conn.commit()
+            return render_template("restaurant.html", prin=f"Your total bill for meal is Rs.{restaurant_bill}/-")
+    return render_template("restaurant.html")
+
+# room rent function
+
+
+def room_rent(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    query1 = f"SELECT ROOMBILL FROM ROOM_RENT WHERE CID = {cid}"
+    mydb.execute(query1)
+    try:
+        roomrent = mydb.fetchone()['ROOMBILL']
+        print(roomrent)
+        return roomrent
+    except:
+        roomrent = 0
+        print(roomrent)
+        return roomrent
+
+# restaurent function
+
+
+def rest(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    query1 = f"SELECT RESTAURANT_BILL FROM RESTAURANT WHERE CID = {cid}"
+    mydb.execute(query1)
+    try:
+        roomrent = mydb.fetchone()['RESTAURANT_BILL']
+        print(roomrent)
+        return roomrent
+    except:
+        roomrent = 0
+        print(roomrent)
+        return roomrent
+
+# hotel total bill
+
+
+@app.route("/hotel/total", methods=['GET', 'POST'])
+def total():
+    if request.method == 'POST':
+        createTable = """CREATE TABLE IF NOT EXISTS TOTAL_AMOUNT(CID VARCHAR(20) PRIMARY KEY,ROOMBILL INT ,RESTAURANT_BILL INT ,GRANDTOTAL INT)"""
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        mydb.execute(createTable)
+        cid = request.form['cid']
+        try:
+
+            roomrent = room_rent(cid)
+            restaurantbill = rest(cid)
+            grandtotal = roomrent + restaurantbill
+            query = f"""INSERT INTO total_amount(cid,roombill,restaurant_bill,grandTotal) VALUES('{cid}',{roomrent},{restaurantbill},{grandtotal})"""
+            mydb.execute(query)
+            conn.commit()
+            return render_template("total.html", prin=f'Your total bill is Rs.{grandtotal}')
+        except:
+            return jsonify({"Error": f"No data found for id: {cid}"})
+    return render_template("total.html")
+
+
+def getname(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    cid = request.form['cid']
+    query = f"""SELECT NAME FROM customer_details WHERE CID = '{cid}'"""
+    mydb.execute(query)
+    name = mydb.fetchone()['NAME']
+    return name
+
+
+def getaddress(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    cid = request.form['cid']
+    query = f"""SELECT ADDRESS FROM customer_details WHERE CID = '{cid}'"""
+    mydb.execute(query)
+    address = mydb.fetchone()['ADDRESS']
+    return address
+
+
+def getage(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    cid = request.form['cid']
+    query = f"""SELECT AGE FROM customer_details WHERE CID = '{cid}'"""
+    mydb.execute(query)
+    age = mydb.fetchone()['AGE']
+    return age
+
+
+def getnation(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    cid = request.form['cid']
+    query = f"""SELECT NATIONALITY FROM customer_details WHERE CID = '{cid}'"""
+    mydb.execute(query)
+    nationality = mydb.fetchone()['NATIONALITY']
+    return nationality
+
+
+def getphone(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    cid = request.form['cid']
+    query = f"""SELECT PHONENO FROM customer_details WHERE CID = '{cid}'"""
+    mydb.execute(query)
+    phoneno = mydb.fetchone()['PHONENO']
+    return phoneno
+
+
+def getemail(cid):
+    conn = connection()
+    mydb = conn.cursor()
+    mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+    mydb.execute("USE schoolproject")
+    cid = request.form['cid']
+    query = f"""SELECT EMAIL FROM customer_details WHERE CID = '{cid}'"""
+    mydb.execute(query)
+    email = mydb.fetchone()['EMAIL']
+    return email
+# Show records
+
+
+@app.route("/hotel/showrecord", methods=['GET', 'POST'])
+def show():
+    if request.method == 'POST':
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        cid = request.form['cid']
+        try:
+            query = f"SELECT cid FROM customer_details WHERE CID = '{cid}'"
+            mydb.execute(query)
+            name = getname(cid)
+            address = getaddress(cid)
+            age = getage(cid)
+            nation = getnation(cid)
+            phone = getphone(cid)
+            email = getemail(cid)
+            return render_template("data.html", para=f'Here are details:', cid=f'Customer Id: {cid}', name=f'Name: {name}', address=f'Address: {address}', age=f'Age: {age}', nation=f'Nationality: {nation}', phone=f'Phone No: {phone}', email=f'Email Id: {email}')
+        except:
+            return jsonify({"Error": f"Data not found for Id: {cid}"})
+    return render_template("data.html")
+
+#delete record from particular table
+@app.route('/hotel/delete', methods=['GET', 'POST'])
+def delete():
+    if request.method == 'POST':
+        conn = connection()
+        mydb = conn.cursor()
+        mydb.execute("CREATE DATABASE IF NOT EXISTS schoolproject")
+        mydb.execute("USE schoolproject")
+        cid = request.form['cid']
+        select = request.form.get("choose")
+        try:
+            if select == 'all':
+                query1 = f"DELETE FROM customer_details WHERE CID='{cid}'"
+                query2 = f"DELETE FROM BOOKING WHERE CID='{cid}'"
+                query3 = f"DELETE FROM RESTAURANT WHERE CID='{cid}'"
+                query4 = f"DELETE FROM room_rent WHERE CID={cid}"
+                query5 = f"DELETE FROM TOTAL_AMOUNT WHERE CID='{cid}'"
+                mydb.execute(query1)
+                mydb.execute(query2)
+                mydb.execute(query3)
+                mydb.execute(query4)
+                mydb.execute(query5)
+                conn.commit()
+                return render_template("delete.html", para = f'Deleted data from all tables for {cid}')
+            elif select == 'bookings':
+                query2 = f"DELETE FROM BOOKING WHERE CID='{cid}'"
+                mydb.execute(query2)
+                conn.commit()
+                return render_template("delete.html", para = f'Deleted data from Booking table for {cid}')
+            elif select == 'customer':
+                query1 = f"DELETE FROM customer_details WHERE CID='{cid}'"
+                mydb.execute(query1)
+                conn.commit()
+                return render_template("delete.html", para = f'Deleted data from Customer Details table for {cid}')
+            elif select == 'restaurant':
+                query3 = f"DELETE FROM RESTAURANT WHERE CID='{cid}'"
+                mydb.execute(query3)
+                conn.commit()
+                return render_template("delete.html", para = f'Deleted data from Restaurant table for {cid}')
+            elif select == 'roomrent':
+                query4 = f"DELETE FROM room_rent WHERE CID={cid}"
+                mydb.execute(query4)
+                conn.commit()
+                return render_template("delete.html", para = f'Deleted data from Room Rent table for {cid}')
+            elif select == 'total':
+                query5 = f"DELETE FROM TOTAL_AMOUNT WHERE CID='{cid}'"
+                mydb.execute(query5)
+                conn.commit()
+                return render_template("delete.html", para = f'Deleted data from Grand Total table for {cid}')
+            else:
+                return render_template("delete.html", para = f'Please select a valid option.')
+
+        except:
+            return jsonify({"Error": "Data not found."})
+    return render_template("delete.html")
+
+
+# redirecting to long link
+@app.route('/<SHORT>')
+def getlink(SHORT):
+    SHORT = str(SHORT)
+    conn = connection()
+    mydb = conn.cursor()
+    try:
+        query = 'SELECT LINK FROM urls WHERE SHORT = %s '
+        mydb.execute(query, (SHORT))
+        redirectURL = mydb.fetchone()['LINK']
+        print(redirectURL)
+        return redirect(redirectURL, code=301)
+    except:
+        return jsonify({"ERROR": 'Data not found!'})
+
+
+# running site
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='10000')
